ee4005e66199ae31f6c2402f7e1ef599e941d0e4
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-12-16 09:10:54 -0500

    e

--- api.py
+++ api.py
@@ -1,1 +1,11 @@
 import requests
+
+def getWeather(weather):
+    instead = requests.get(f"https://www.wunderground.com/forecast/us/ny/new-york-city")
+    if instead.stautus_code != 200:
+        print("Error")
+        return None
+    
+    data = instead.json()
+    return {
+        temperature = data[temperature]
