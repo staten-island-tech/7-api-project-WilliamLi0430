0f02cc31a3671b12b7caf141800a40ebf6e3767b
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-12-17 09:26:07 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,15 +1,18 @@
 import requests
 
 def getWeather(weather):
-    instead = requests.get(f"https://www.wunderground.com/forecast/us/ny/new-york-city")
-    if instead.stautus_code != 200:
+    instead = requests.get(f"https://developer.accuweather.com/core-weather/location-key-currentconditions")
+    if instead.status_code != 200:
         print("Error")
         return None
     
     data = instead.json()
     return {
-        "temperature": data["temperature"],
+        "temperature": data["Temperature"],
+        "wind": data["Wind"],
+        "humidity": data["Humidity"],
+        "visibility": data["Visibility"]
     }
 
-weather = getWeather("")
+weather = getWeather("temperature")
 print(weather)
