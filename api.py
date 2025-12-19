7f3cec42a527056257e36a5204f7edc55b19329e
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-12-19 09:26:05 -0500

    e

--- api.py
+++ api.py
@@ -1,17 +1,16 @@
 import requests
 
 def getWeather(weather):
-    instead = requests.get(f"https://developer.accuweather.com/core-weather/location-key-currentconditions")
+    instead = requests.get(f"https://dataservice.accuweather.com/{weather.lower()}]")
     if instead.status_code != 200:
         print("Error")
         return None
     
-    data = instead.json()
+    data = instead.json()[0]
     return {
-        "temperature": data["Temperature"],
-        "wind": data["Wind"],
-        "humidity": data["Humidity"],
-        "visibility": data["Visibility"]
+        "temperature": data["Temperature"]["Metric"]["Imperial"],
+        "wind": data["Wind"]["Direction"]["Speed"],
+        "humidity": data["RelativeHumidity"],
     }
 
 weather = getWeather("temperature")
