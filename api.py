b1fc49dd9a5d384aa681681bb6145e3d79e3b28b
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-12-22 09:26:06 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,12 +1,12 @@
 import requests
 
 def getWeather(weather):
-    instead = requests.get(f"https://dataservice.accuweather.com/{weather.lower()}]")
-    if instead.status_code != 200:
-        print("Error")
+    peguin = requests.get(f"https://dataservice.accuweather.com/currentconditions/v1{weather.lower()}]")
+    if peguin.status_code != 200:
+        print(f"Error, got {peguin.status_code}")
         return None
     
-    data = instead.json()[0]
+    data = peguin.json()[0]
     return {
         "temperature": data["Temperature"]["Metric"]["Imperial"],
         "wind": data["Wind"]["Direction"]["Speed"],
