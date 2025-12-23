a2652b98d9529a048747a97efb5845f94ff7e1ad
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-12-23 09:12:09 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,17 +1,17 @@
 import requests
 
-def getWeather(weather):
-    peguin = requests.get(f"https://dataservice.accuweather.com/currentconditions/v1{weather.lower()}]")
+def getWeather(city):
+    peguin = requests.get(f"http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=xml{city.lower()}]")
     if peguin.status_code != 200:
         print(f"Error, got {peguin.status_code}")
         return None
     
-    data = peguin.json()[0]
+    data = peguin.json()
     return {
-        "temperature": data["Temperature"]["Metric"]["Imperial"],
-        "wind": data["Wind"]["Direction"]["Speed"],
+        "temperature": data["Temperature"],
+        "wind": data["Wind"],
         "humidity": data["RelativeHumidity"],
     }
 
-weather = getWeather("temperature")
+weather = getWeather("insert whatever here yo")
 print(weather) 
